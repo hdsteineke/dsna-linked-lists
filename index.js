1a462bb3d88a046b0489a44492812d513d69fa66
HAILEY STEINEKE <hailey.steineke@gmail.com>
2022-06-28 10:26:12 -0700

    filled in linked-list-add challenge

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+// creating a Node class that has a value of something and a 'next' value = null
 class Node {
   constructor(val) {
     this.val = val;
@@ -5,15 +6,27 @@ class Node {
   }
 }
 
+// creating a LinkedList class that has a root value of 'new Node'
 class LinkedList {
   constructor(val) {
     this.head = new Node(val);
   }
 
-  add(){
-
+  
+  add(val){
+    // sets the current value to the root
+    let current  = this.head;
+    // checks if current value has a next value
+    while (current.next) {
+      //if it does have a next value, sets 'current' = 'next' value
+      current = current.next;
+    }
+    //adds the current.next value onto the LinkedList as new Node, where next value is null (??)
+    current.next = new Node(val);
   }
-  removeTail(){}
+
+
+  //removeTail(){}
 
   getList() {
     const listArr = [];
@@ -32,6 +45,10 @@ console.log(list.head);
 list.add("B");
 console.log(list.getList());
 list.add("C");
+console.log(list.getList());
+list.add("D");
+console.log(list.getList());
+list.add("E");
 console.log(list.getList());
 
 module.exports = { LinkedList };
